168fc7a38c36b14fc036985482db1a12624f26ff
Simon M <9540040+syydot@users.noreply.github.com>
2018-05-26 10:45:50 +0200

    added new stuff

---
+++ config/polybar/startup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# polybar beenden, wenn sie bereits laeuft
+killall -q polybar
+
+# solange der prozess noch laeuft
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# bar 
+polybar nms_synth
